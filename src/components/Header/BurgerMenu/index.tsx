ddbb64fe4e8a9cf4dd4ab8ce2c2f19a0b7d3f175
Yevhenii <ke1vin.kapustian@gmail.com>
2025-08-13 13:18:45 +0200

    Added tests

--- src/components/Header/BurgerMenu/index.tsx
+++ src/components/Header/BurgerMenu/index.tsx
@@ -14,8 +14,8 @@ const BurgerMenu = () => {
     }
 
     return(
-        <div className="hidden max-sm:block">
-            <button onClick={handleClick} className="cursor-pointer">
+        <div data-testid="burgerWrapper" className="hidden max-sm:block">
+            <button aria-label="openButton" onClick={handleClick} className="cursor-pointer">
                 <List size={32}/>
             </button>
             <AnimatePresence>
@@ -26,7 +26,7 @@ const BurgerMenu = () => {
                                 exit={{ x: 300, opacity: 0 }}
                                 transition={{ duration: 0.5, ease: "easeInOut" }}
                                 className="w-1/2 h-screen fixed top-0 right-0 flex flex-col items-center justify-center gap-5 bg-white">
-                        <button className="absolute top-5 left-5 cursor-pointer" onClick={handleClick}>
+                        <button aria-label="closeButton" className="absolute top-5 left-5 cursor-pointer" onClick={handleClick}>
                             <XCircle color="black" size={32}/>
                         </button>
                         {navigationLinks.map((item, index) => <li className=" text-black" key={index}><Navigation {...item}/></li> )}
